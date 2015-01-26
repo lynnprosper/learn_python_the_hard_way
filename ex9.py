ec756e719c26450c781d5a5d6f3b1412559663f9
Miles <lynnprosper@gmail.com>
2015-01-26 23:50:46 +0800

    ex11
    input() , %r for debugging

--- ex9.py
+++ ex9.py
@@ -1,3 +1,8 @@
-while True:
-	for i in ["/", "-", "|", "\\", "|"]:
-		print ("%s\r" % i)
+print ("How old are you?")
+age = input()
+print ("How tall are you?")
+height = input()
+print ("How much do you weigh?")
+weight = input()
+
+print ("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
