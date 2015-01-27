5d886f9845e89748d0a639407c5abd0b0032e6e3
Miles <lynnprosper@gmail.com>
2015-01-27 23:51:33 +0800

    ex13
    parameters, unpacking, variables

--- ex9.py
+++ ex9.py
@@ -1,5 +1,7 @@
-age = input("How old are you?\n")
-height = input("How tall are you? ")
-weight = input("How much do you weight? ")
+from sys import argv
 
-print ("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
+script, first, second, third = argv
+print ("The script is called:", script)
+print ("Your first variable is:", first)
+print ("Your second variable is:", second)
+print ("Your third variable is:", third)
