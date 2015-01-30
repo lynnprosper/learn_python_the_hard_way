a0b81d9e6a01711d21d22face4ab44bf38910063
Miles <lynnprosper@gmail.com>
2015-01-30 23:48:35 +0800

    ex16
    target.write()

--- ex9.py
+++ ex9.py
@@ -1,14 +1,33 @@
 from sys import argv
 
-scirpt, filename = argv
+script, filename = argv
 
-txt = open(filename)
+print ("We're going to erase %r." % filename)
+print ("If you don't want that, hit CTRL-C (^C).")
+print ("If you do want that, hit RETURN.")
 
-print ("Here's your file %r:" % filename)
-print (txt.read())
+input("?")
 
-print ("Type the filename again:")
-file_again = input(">")
+print ("Opening the file...")
+target = open(filename, 'w')
 
-txt_again = open(file_again)
-print (txt_again.read())
+print ("Truncating the file. Goodbye!")
+target.truncate()
+
+print ("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print ("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write("line3")
+target.write("\n")
+
+print ("And finally, we close it.")
+target.close()
