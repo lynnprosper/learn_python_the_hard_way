35a26585096b3f61a2a7fae8c27f240fce2a81e1
Miles <lynnprosper@gmail.com>
2015-01-31 23:37:04 +0800

    ex17
    os.path,  exists.
    copy files.

--- ex9.py
+++ ex9.py
@@ -1,23 +1,23 @@
 from sys import argv
+from os.path import exists
 
-script, filename = argv
+script, from_file, to_file = argv
 
-print ("We're going to erase %r." % filename)
-print ("If you don't want that, hit CTRL-C (^C).")
-print ("If you do want that, hit RETURN.")
+print ("Copying from %s to %s" % (from_file, to_file))
 
-input("?")
+in_file = open(from_file)
+indata = in_file.read()
 
-print ("Opening the file...")
-target = open(filename, 'w')
+print ("The input file is %d bytes long" % len(indata))
 
-print ("Truncate the file. Goodbye!")
-target.truncate()
+print ("Does the output file exist? %r" %exists(to_file))
+print ("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
 
-print ("Now I'm going to ask you for three lines.")
-line1 = input("line 1: ")
-line2 = input("line 2: ")
-line3 = input("line 3: ")
+out_file = open(to_file, 'w')
+out_file.write(indata)
 
-print ("I'm going to write these to the file.")
-target.write(' %s\n %s\n %s\n' % (line1, line2 ,line3))
+print ("Alright, all done.")
+
+out_file.close()
+in_file.close()
