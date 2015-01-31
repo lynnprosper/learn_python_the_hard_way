e627a729b3e258ba084e7bba6025cb994df1b1b0
Miles <lynnprosper@gmail.com>
2015-01-31 23:43:12 +0800

    ex17_1
    simplify thecodes.

--- ex9.py
+++ ex9.py
@@ -5,8 +5,7 @@ script, from_file, to_file = argv
 
 print ("Copying from %s to %s" % (from_file, to_file))
 
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
 print ("The input file is %d bytes long" % len(indata))
 
@@ -14,10 +13,6 @@ print ("Does the output file exist? %r" %exists(to_file))
 print ("Ready, hit RETURN to continue, CTRL-C to abort.")
 input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+out_file = open(to_file, 'w').write(indata)
 
 print ("Alright, all done.")
-
-out_file.close()
-in_file.close()
