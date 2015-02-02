f27943e5866a070a0152fabaff5de537c9031445
Miles <lynnprosper@gmail.com>
2015-02-02 23:50:03 +0800

    ex19
    functions, variables.call run

--- ex9.py
+++ ex9.py
@@ -1,17 +1,19 @@
-def print_two(*args):
-	arg1, arg2 = args
-	print ("arg1: %r, arg2: %r" % (arg1, arg2))
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+	print("You have %d cheeses!" % cheese_count)
+	print("You have %d boxes of crackers!" % boxes_of_crackers)
+	print("Man that's enough for a party!")
+	print("Get a blanket.\n")
 	
-def print_two_again(arg1, arg2):
-	print("arg1: %r, arg2: %r" % (arg1, arg2))
-	
-def print_one(arg1):
-	print("arg1: %r" % arg1)
-	
-def print_none():
-	print("I got nothin'.")
-	
-print_two("Zed", "Shaw")
-print_two_again("Zed", "Shaw")
-print_one("First!")
-print_none()
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
+
+print("OR, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print("We can even do math inside too:")
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print("And we can combine the two, variables and maths:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 50)
