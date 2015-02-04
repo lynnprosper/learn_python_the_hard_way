0bee4860dc8cc097a7bb51e487d4a1cf821a2f1f
Miles <lynnprosper@gmail.com>
2015-02-05 00:01:02 +0800

    ex21
    fuctions,  variables

--- ex9.py
+++ ex9.py
@@ -1,35 +1,30 @@
-from sys import argv
-
-script, input_file = argv
-
-def print_all(f):
-	print(f.read())
-
-def rewind(f):
-	f.seek(0)
-# moving to the start of file.
-# going to the 0 byte in the file.
+def add(a, b):
+	print("ADDING %d + %d" % (a, b))
+	return a + b
 	
-def print_a_line(line_count, f):
-	print (line_count, f.readline())
+def subtract(a, b):
+	print("SUBTRACTING %d - %d" % (a, b))
+	return a - b
 	
-current_file = open(input_file)
+def multiply(a, b):
+	print("MULTIPLYING %d * %d" % (a, b))
+	return a * b 
+	
+def divide(a, b):
+	print("DIVIDING %d / %d" % (a, b))
+	return a / b 
+	
+print("Let's do some math with just functions!")
 
-print("First let's print the whole file:\n")
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
 
-print_all(current_file)
+print("Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq))
 
-print("Now let's rewind, kind of like a tape .")
+print("Here is a puzzle.")
 
-rewind(current_file)
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
-print("Let's print three lines:")
-
-current_line = 1
-print_a_line(current_line, current_file)
-
-current_line = current_line + 2
-print_a_line(current_line, current_file)
-
-#current_line = current_line + 1
-#print_a_line(current_line, current_file)
+print("That becomes: ", what, "Can you do it by hand")
