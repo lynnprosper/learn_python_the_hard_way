ceeb415109b882a799073308de3de920a712aca2
Miles <lynnprosper@gmail.com>
2015-02-06 21:34:27 +0800

    ex25
    functions.

--- ex9.py
+++ ex9.py
@@ -1,35 +1,31 @@
-print("Let's practice everything.")
-print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
-
-poem = """
-\tThe lovely world
-with logic so firmly planted
-cannot discern \n the needs of love
-nor comprehend passion from intuition
-and requires an explanation
-\n\t\twhere there is none.
-"""
-
-print("---------------")
-print(poem)
-print("------------------")
-
-five = 10 - 2 + 3 - 6
-print("This should be five: %s" % five)
-
-def secret_formula(started):
-	jelly_beans = started * 500
-	jars = jelly_beans / 1000
-	crates = jars / 100
-	return jelly_beans, jars, crates
+def break_words(stuff):
+	words = stuff.split(' ')
+	return words
 	
-start_point = 10000
-beans, jars, crates = secret_formula(start_point)
-
-print("With a starting point of: %d" % start_point)
-print("We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates))
-
-start_point = start_point / 10
-
-print("We can also do that this way:")
-print("We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point))
+def sort_words(words):
+	return sorted(words)
+	
+def print_first_word(words):
+# Prints the first word after popping it off.
+	word = words.pop(0)
+	print(word)
+	
+def print_last_word(words):
+# Prints the last word after popping it off.
+	word = words.pop(-1)
+	print(word)
+	
+def sort_sentence(sentence):
+# Takes in a full sentence and returns the sorted words.
+	words = break_words(sentence)
+	return sort_words(words)
+	
+def print_first_and_last(sentence):
+	words = break_words(sentence)
+	print_first_word(words)
+	print_last_word(words)
+	
+def print_first_and_last_sorted(sentence):
+	words = sort_sentence(sentence)
+	print_first_word(words)
+	print_last_word(words)
