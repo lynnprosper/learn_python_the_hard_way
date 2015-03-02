6b8c1badcd8e063efc2bf3b35e3c97858baabe9e
Miles <lynnprosper@gmail.com>
2015-03-02 18:47:31 +0800

    ex33
    while loops

--- ex29.py
+++ ex29.py
@@ -1,20 +1,13 @@
-the_count = [1, 2, 3, 4, 5]
-fruits = ['apples', 'oranges', 'pears', 'apricots']
-change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+i = 0
+numbers = []
 
-for number in the_count:
-	print("This is count %d" % number)
-
-for fruit in fruits:
-	print("A fruit of type: %s" % fruit)
+for i in range(0, 7):
+	print("Ar the top i is %d" % i)
+	numbers.append(i)
+	i += 1
+	print("Numbers now: ", numbers)
+	print("At the bottom i is %d" % i)
 	
-for i in change:
-	print("I got %r" % i)
-	
-elements = []
-for i in range(0, 6):
-	print("Adding %d to the list." % i)
-	elements.append(i)
-	
-for i in elements:
-	print("Element was: %d" % i)
+print("The numbers: ")
+for num in numbers:
+	print(num)
