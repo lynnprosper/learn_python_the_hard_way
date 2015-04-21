108ba95eeb0d24aa66eb8ad4d24211f58f3a3614
Miles <lynnprosper@gmail.com>
2015-04-22 07:33:27 +0800

    ex40
    modules, classes, and objects.

--- ex29.py
+++ ex29.py
@@ -1,49 +1,16 @@
-states = {
-'Oregon' : 'OR',
-'Florida': 'FL',
-'California': 'CA',
-'New York': 'NY',
-'Michigan': 'MI'
-}
+class Song(object):
+	def __init__(self, lyrics):
+		self.lyrics = lyrics
+	def sing_me_a_song(self):
+		for line in self.lyrics:
+			print (line)
+			
+					"I don't want to get sued",
+					"So I'll stop right there"])
+					
+bulls_on_parade = Song(["They rally around the family",
+						"With pockets full of shells"])
+						
+happy_bday.sing_me_a_song()
 
-cities = {
-'CA': 'San Francisco',
-'MI': 'Detroit',
-'FL': 'Jacksonville'
-}
-
-cities['NY'] = 'New York'
-cities['OR'] = 'Portland'
-
-print ('-' * 10)
-print ("NY State has: ", cities['NY'])
-print ("OR State has: ", cities['OR'])
-
-print ('-' * 10)
-print ("Michigan's abbreviation is: ", states['Michigan'])
-print ("Florida's abbreviation is: ", states['Florida'])
-
-print ('-' * 10)
-print ("Michigan has: ", cities[states['Michigan']])
-print ("Florida has: ", cities[states['Florida']])
-
-print ('-' * 10)
-for state, abbrev in states.items():
-	print ("%s is abbreviated %s" % (state, abbrev))
-	
-print ('-' * 10)
-for abbrev, city in cities.items():
-	print ("%s has the city %s" % (abbrev, city))
-	
-print ('-' * 10)
-for state, abbrev in states.items():
-	print ("%s state is abbreviated %s and has city %s" % (state, abbrev, cities[abbrev]))
-	
-print ('-' * 10)
-state = states.get('Texas', None)
-
-if not state:
-	print ("Sorry, no Texas.")
-	
-city = cities.get('TX', 'Does Not Exist')
-print ("The city for the state 'TX' is: %s" % city)
+bulls_on_parade.sing_me_a_song()
