f9111f31cce73d4f7c4460660cb6b5829a571d49
Miles <lynnprosper@gmail.com>
2015-05-06 08:20:51 +0800

    EX50
    web

--- ex50/gothonweb/bin/app.py
+++ ex50/gothonweb/bin/app.py
@@ -4,10 +4,11 @@ urls = (
 )
 
 app = web.application(urls, globals())
+render = web.template.render('templates/')
 
-class index:
+class index(object):
 	def GET(self):
-		#greeting = "Hello World"
-		return greeting
+		greeting = "Hello World"
+		return render.index(greeting = greeting)
 if __name__ == "__main__":
 	app.run()
