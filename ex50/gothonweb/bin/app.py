cd57203b36fe66e6bbd9668ef3d6816af9a433e2
Miles <lynnprosper@gmail.com>
2015-05-14 12:42:29 +0800

    games_finished

--- ex50/gothonweb/bin/app.py
+++ ex50/gothonweb/bin/app.py
@@ -1,17 +1,40 @@
 import web
+import map
+
 urls = (
-   '/hello', 'Index'
+   '/game', 'GameEngine',
+   '/', 'Index',
 )
 
 app = web.application(urls, globals())
+if web.config.get('_session') is None:
+	store = web.session.DiskStore('sessions')
+	session = web.session.Session(app, store,
+								  initializer = {'room': None})
+	web.config._session = session
+else:
+	session = web.config._session
 render = web.template.render('templates/', base="layout")
+
 class Index(object):
 	def GET(self):
-		return render.hello_form_laid_out()
-	
+		session.room = map.START
+		web.seeother("/game")
+		
+class GameEngine(object):
+	def GET(self):
+		if session.room:
+			return render.show_room(room=session.room)
+		else:
+			return render.you_died()
+			
 	def POST(self):
-		form = web.input(name="Nobody", greet="Hello")
-		greeting = "%s, %s" % (form.greet, form.name)
-		return render.index_laid_out(greeting = greeting)
+		form = web.input(action=None)
+		
+		if session.room and form.action:
+			session.room = session.room.go(form.action)
+		web.seeother("/game")
+	
+	
 if __name__ == "__main__":
 	app.run()
--- ex50/gothonweb/map.py
+++ ex50/gothonweb/map.py
@@ -37,3 +37,25 @@ the_end_loser = Room("The End",
 """
 You jump into a random pod
 """)
+
+escape_pod.add_paths({
+	'2': the_end_winner,
+	'*': the_end_loser
+})
+
+generic_death = Room("death", "you died.")
+
+the_bridge.add_paths({
+	'throw the bomb': generic_death,
+	'slowly place the bomb': escape_pod})
+	
+laser_weapon_armory.add_paths({
+	'0132': the_bridge,
+	'*': generic_death})
+	
+central_corridor.add_paths({
+	'shoot!': generic_death,
+	'dodge!': generic_death,
+	'tell a joke': laser_weapon_armory})
+	
+START = central_corridor
